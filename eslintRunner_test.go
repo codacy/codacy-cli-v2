9705ab313dd8e4bf69564029d593d18d8732d2e0
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2024-05-21 13:36:02 +0200

    feature: Add method to run eslint
    Co-authored-by: Joao Machado <machadoit@users.noreply.github.com>

---
+++ eslintRunner_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRunEslint(t *testing.T) {
+	homeDirectory, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	currentDirectory, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	testDirectory := "testdata/repositories/test1"
+	repositoryToAnalyze := filepath.Join(testDirectory, "src")
+	sarifOutputFile := filepath.Join(testDirectory, "sarif.json")
+	eslintInstallationDirectory := filepath.Join(homeDirectory, ".cache/codacy-cli-v2/tools/eslint")
+	nodeBinary := "node"
+
+	eslintOutput, err := runEslint(repositoryToAnalyze, eslintInstallationDirectory, nodeBinary)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	expectedSarifBytes, err := os.ReadFile(sarifOutputFile)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	filePrefix := "file://" + currentDirectory
+	actualSarif := strings.ReplaceAll(eslintOutput, filePrefix, "")
+
+	expectedSarif := string(expectedSarifBytes)
+
+	assert.Equal(t, expectedSarif, actualSarif, "output did not match expected")
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,11 @@ go 1.22.3
 require github.com/spf13/cobra v1.8.0
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/bodgit/plumbing v1.2.0 // indirect
 	github.com/bodgit/sevenzip v1.3.0 // indirect
@@ -25,6 +30,7 @@ require (
 	github.com/nwaples/rardecode/v2 v2.0.0-beta.2 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.9.0
 	github.com/therootcompany/xz v1.0.1 // indirect
 	github.com/ulikunitz/xz v0.5.10 // indirect
 	go4.org v0.0.0-20200411211856-f5505b9728dd // indirect
