5155e9d259b26e0845d9fcf0d708d6a6c36d320f
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2024-05-22 18:18:39 +0200

    Use current latest tag for brew

--- Formula/codacy-cli-v2.rb
+++ Formula/codacy-cli-v2.rb
@@ -1,6 +1,6 @@
 class CodacyCliV2 < Formula
-  version "1.0.0"
-  url "https://raw.githubusercontent.com/codacy/codacy-cli-v2/8ce8c5da6ee6cf3ff145b7b18f861a6d740e11c0/codacy-cli.sh"
+  version "0.1.0-main.35.70f0f48"
+  url "https://raw.githubusercontent.com/codacy/codacy-cli-v2/0.1.0-main.35.70f0f48/codacy-cli.sh"
   sha256 "fb616e2f5c639985566c81a6e6ce51db2e8de56bf217e837d13efe2f3ccc3042"
 
   def install
