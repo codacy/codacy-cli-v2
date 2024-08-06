677c42dfcd828a9cdede760b962fe167533cc512
Stefan Vacareanu <stefan@codacy.com>
2024-08-06 10:22:44 +0300

    Version2

--- cli-v2.go
+++ cli-v2.go
@@ -1,22 +1,375 @@
 package main
 
 import (
+	"bytes"
 	"codacy/cli-v2/cmd"
 	"codacy/cli-v2/config"
 	cfg "codacy/cli-v2/config-file"
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"strings"
 )
 
+type Sarif struct {
+	Runs []struct {
+		Tool struct {
+			Driver struct {
+				Name    string `json:"name"`
+				Version string `json:"version"`
+				Rules   []struct {
+					ID               string `json:"id"`
+					HelpURI          string `json:"helpUri"`
+					ShortDescription struct {
+						Text string `json:"text"`
+					} `json:"shortDescription"`
+				} `json:"rules"`
+			} `json:"driver"`
+		} `json:"tool"`
+		Artifacts []struct {
+			Location struct {
+				URI string `json:"uri"`
+			} `json:"location"`
+		} `json:"artifacts"`
+		Results []struct {
+			Level   string `json:"level"`
+			Message struct {
+				Text string `json:"text"`
+			} `json:"message"`
+			Locations []struct {
+				PhysicalLocation struct {
+					ArtifactLocation struct {
+						URI   string `json:"uri"`
+						Index int    `json:"index"`
+					} `json:"artifactLocation"`
+					Region struct {
+						StartLine   int `json:"startLine"`
+						StartColumn int `json:"startColumn"`
+						EndLine     int `json:"endLine"`
+						EndColumn   int `json:"endColumn"`
+					} `json:"region"`
+				} `json:"physicalLocation"`
+			} `json:"locations"`
+			RuleID    string `json:"ruleId"`
+			RuleIndex int    `json:"ruleIndex"`
+		} `json:"results"`
+	} `json:"runs"`
+}
+
+type CodacyPattern struct {
+	UUID        string `json:"uuid"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Category    string `json:"category"`
+	Description string `json:"description"`
+}
+
+type CodacyPatternResponse struct {
+	Data []CodacyPattern `json:"data"`
+}
+
+type CodacyIssue struct {
+	Source   string `json:"source"`
+	Line     int    `json:"line"`
+	Type     string `json:"type"`
+	Message  string `json:"message"`
+	Level    string `json:"level"`
+	Category string `json:"category"`
+}
+
+type CodacyPayload struct {
+	Tool struct {
+		Name string `json:"name"`
+	} `json:"tool"`
+	Issues []CodacyIssue `json:"issues"`
+}
+
+var codacyPatterns []CodacyPattern
+
 func main() {
+
+	fmt.Println("Running original CLI functionality...")
+	// Original functionality
 	config.Init()
 
 	configErr := cfg.ReadConfigFile(config.Config.ProjectConfigFile())
 	// whenever there is no configuration file, the only command allowed to run is the 'init'
-	if configErr != nil && os.Args[1] != "init" {
+	if configErr != nil && len(os.Args) > 1 && os.Args[1] != "init" {
 		fmt.Println("No configuration file was found, execute init command first.")
 		return
 	}
 
 	cmd.Execute()
+	var sarifPath, commitUuid, projectToken, baseDir, apiToken string
+	flag.StringVar(&sarifPath, "sarif-path", "", "Path to the SARIF report")
+	flag.StringVar(&commitUuid, "commit-uuid", "", "Commit UUID")
+	flag.StringVar(&projectToken, "project-token", "", "Project token for Codacy API")
+	flag.StringVar(&baseDir, "basedir", "", "Base directory where code is cloned")
+	flag.StringVar(&apiToken, "api-token", "", "API token for Codacy")
+	flag.Parse()
+
+	if commitUuid != "" && projectToken != "" && baseDir != "" && apiToken != "" {
+		fmt.Println("Processing SARIF and sending results...")
+		processSarifAndSendResults(sarifPath, commitUuid, projectToken, baseDir, apiToken)
+		return
+	}
+}
+
+func processSarifAndSendResults(sarifPath, commitUuid, projectToken, baseDir, apiToken string) {
+	fmt.Printf("Loading SARIF file from path: %s\n", sarifPath)
+	// Load SARIF file
+	sarifFile, err := os.Open(sarifPath)
+	if err != nil {
+		fmt.Printf("Error opening SARIF file: %v\n", err)
+		os.Exit(1)
+	}
+	defer sarifFile.Close()
+
+	var sarif Sarif
+	fmt.Println("Parsing SARIF file...")
+	err = json.NewDecoder(sarifFile).Decode(&sarif)
+	if err != nil {
+		fmt.Printf("Error parsing SARIF file: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Loading Codacy patterns...")
+	// Load Codacy patterns
+	loadCodacyPatterns(apiToken)
+
+	fmt.Println("Processing SARIF results...")
+	// Process SARIF results
+	codacyIssues := processSarif(sarif, baseDir)
+
+	fmt.Println("Filtering issues...")
+	// Filter issues
+	filterIn, _ := filterPatterns(codacyIssues)
+
+	fmt.Println("Sending results to Codacy...")
+	// Send results to Codacy
+	sendResults(filterIn, commitUuid, projectToken)
+}
+
+// Helper function to extract cursor from JSON response body
+func extractCursorFromResponseBody(body io.Reader) (string, error) {
+	var response struct {
+		Pagination struct {
+			Cursor string `json:"cursor"`
+		} `json:"pagination"`
+	}
+
+	err := json.NewDecoder(body).Decode(&response)
+	if err != nil {
+		return "", fmt.Errorf("error decoding response body: %w", err)
+	}
+	return response.Pagination.Cursor, nil
+}
+
+func loadCodacyPatterns(apiToken string) {
+	fmt.Println("Fetching Codacy patterns...")
+
+	var cursor string
+	const baseURL = "https://app.codacy.com/api/v3/tools/f8b29663-2cb2-498d-b923-a10c6a8c05cd/patterns"
+
+	fmt.Printf("Requesting patterns from URL: %s\n", baseURL)
+	req, err := http.NewRequest("GET", baseURL, nil)
+	if err != nil {
+		fmt.Printf("Error creating request: %v\n", err)
+		os.Exit(1)
+	}
+	req.Header.Set("api-token", apiToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Printf("Error fetching patterns: %v\n", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error fetching patterns, status code: %d\n", resp.StatusCode)
+		os.Exit(1)
+	}
+
+	// Read the response body into a buffer
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error reading response body: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Decode the body into patternResp
+	var patternResp struct {
+		Data []CodacyPattern `json:"data"`
+	}
+	fmt.Println("Decoding pattern response...")
+	err = json.Unmarshal(body, &patternResp)
+	if err != nil {
+		fmt.Printf("Error decoding pattern response: %v\n", err)
+		os.Exit(1)
+	}
+
+	patternCount := len(patternResp.Data)
+	fmt.Printf("Loaded %d patterns.\n", patternCount)
+	codacyPatterns = append(codacyPatterns, patternResp.Data...)
+
+	// Extract cursor from the body
+	cursor, err = extractCursorFromResponseBody(bytes.NewReader(body))
+	if err != nil {
+		fmt.Printf("Error extracting cursor: %v\n", err)
+		os.Exit(1)
+	}
+
+	for {
+		// Construct the URL with the cursor if it exists
+		url := baseURL
+		if cursor != "" {
+			url += "?cursor=" + cursor
+		}
+
+		fmt.Printf("Requesting patterns from URL: %s\n", url)
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			fmt.Printf("Error creating request: %v\n", err)
+			os.Exit(1)
+		}
+		req.Header.Set("api-token", apiToken)
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Printf("Error fetching patterns: %v\n", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			fmt.Printf("Error fetching patterns, status code: %d\n", resp.StatusCode)
+			os.Exit(1)
+		}
+
+		// Read the response body into a buffer
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("Error reading response body: %v\n", err)
+			os.Exit(1)
+		}
+
+		// Decode the body into patternResp
+		var patternResp struct {
+			Data []CodacyPattern `json:"data"`
+		}
+		fmt.Println("Decoding pattern response...")
+		err = json.Unmarshal(body, &patternResp)
+		if err != nil {
+			fmt.Printf("Error decoding pattern response: %v\n", err)
+			os.Exit(1)
+		}
+
+		patternCount := len(patternResp.Data)
+		fmt.Printf("Loaded %d patterns.\n", patternCount)
+		codacyPatterns = append(codacyPatterns, patternResp.Data...)
+
+		// Extract cursor from the body
+		cursor, err = extractCursorFromResponseBody(bytes.NewReader(body))
+		if err != nil {
+			fmt.Printf("Error extracting cursor: %v\n", err)
+			os.Exit(1)
+		}
+
+		if cursor == "" {
+			fmt.Println("No more patterns to load.")
+			break
+		}
+	}
+}
+
+func processSarif(sarif Sarif, baseDir string) []CodacyIssue {
+	fmt.Println("Processing SARIF results...")
+	var codacyIssues []CodacyIssue
+
+	for _, run := range sarif.Runs {
+		for _, result := range run.Results {
+			for _, location := range result.Locations {
+				source := strings.Replace(location.PhysicalLocation.ArtifactLocation.URI, "file://", "", 1)
+				source = strings.Replace(source, baseDir, "", 1)
+
+				codacyIssues = append(codacyIssues, CodacyIssue{
+					Source:  source,
+					Line:    location.PhysicalLocation.Region.StartLine,
+					Type:    result.RuleID,
+					Message: result.Message.Text,
+					Level:   result.Level,
+				})
+			}
+		}
+	}
+
+	fmt.Printf("Processed %d issues from SARIF.\n", len(codacyIssues))
+	fmt.Print(codacyIssues)
+	return codacyIssues
+}
+
+func filterPatterns(issues []CodacyIssue) ([]CodacyIssue, []CodacyIssue) {
+	fmt.Println("Filtering issues based on Codacy patterns...")
+	var filterIn, filterOut []CodacyIssue
+
+	codacyPatternMap := make(map[string]CodacyPattern)
+	for _, pattern := range codacyPatterns {
+		codacyPatternMap[pattern.ID] = pattern
+	}
+
+	for _, issue := range issues {
+		if pattern, exists := codacyPatternMap[issue.Type]; exists {
+			issue.Type = pattern.ID
+			filterIn = append(filterIn, issue)
+		} else {
+			filterOut = append(filterOut, issue)
+		}
+	}
+
+	fmt.Printf("Filtered %d issues in and %d issues out.\n", len(filterIn), len(filterOut))
+	return filterIn, filterOut
+}
+
+func sendResults(issues []CodacyIssue, commitUuid, projectToken string) {
+	fmt.Println("Sending issues to Codacy...")
+	payload := CodacyPayload{
+		Tool: struct {
+			Name string `json:"name"`
+		}{Name: "ESLint"},
+		Issues: issues,
+	}
+
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Printf("Error marshaling payload: %v\n", err)
+		os.Exit(1)
+	}
+
+	url := fmt.Sprintf("https://api.codacy.com/2.0/commit/%s/issuesRemoteResults", commitUuid)
+	fmt.Printf("Sending results to URL: %s\n", url)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		fmt.Printf("Error creating request: %v\n", err)
+		os.Exit(1)
+	}
+	req.Header.Set("content-type", "application/json")
+	req.Header.Set("project-token", projectToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Printf("Error sending results: %v\n", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error sending results, status code: %d\n", resp.StatusCode)
+		os.Exit(1)
+	}
+
+	fmt.Println("Results sent successfully.")
 }
--- go.mod
+++ go.mod
@@ -2,11 +2,13 @@ module codacy/cli-v2
 
 go 1.22.3
 
-require github.com/spf13/cobra v1.8.0
+require (
+	github.com/google/uuid v1.6.0
+	github.com/spf13/cobra v1.8.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 )
 
@@ -17,17 +19,13 @@ require (
 	github.com/bodgit/windows v1.0.0 // indirect
 	github.com/connesc/cipherio v0.2.1 // indirect
 	github.com/dsnet/compress v0.0.1 // indirect
-	github.com/fatih/color v1.17.0 // indirect
-	github.com/goccy/go-yaml v1.11.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/klauspost/pgzip v1.2.5 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mholt/archiver/v4 v4.0.0-alpha.8 // indirect
+	github.com/mholt/archiver/v4 v4.0.0-alpha.8
 	github.com/nwaples/rardecode/v2 v2.0.0-beta.2 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
@@ -35,8 +33,6 @@ require (
 	github.com/therootcompany/xz v1.0.1 // indirect
 	github.com/ulikunitz/xz v0.5.10 // indirect
 	go4.org v0.0.0-20200411211856-f5505b9728dd // indirect
-	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.3.8 // indirect
-	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1
 )
