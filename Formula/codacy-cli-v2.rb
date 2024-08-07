6113cd1c00e784ae195b19846ebc39f85c87997c
Stefan Vacareanu <stefan@codacy.com>
2024-08-07 15:12:29 +0300

    update sha

--- Formula/codacy-cli-v2.rb
+++ Formula/codacy-cli-v2.rb
@@ -1,7 +1,7 @@
 class CodacyCliV2 < Formula
   version "0.1.0-main.84.d83aad7"
   url "https://raw.githubusercontent.com/codacy/codacy-cli-v2/0.1.0-main.84.d83aad7/codacy-cli.sh"
-  sha256 "fb616e2f5c639985566c81a6e6ce51db2e8de56bf217e837d13efe2f3ccc3042"
+  sha256 "c295066ba1237eb40439a5c04bda42775d0a5bbf1a270cdf05628d307c40f527"
 
   def install
     bin.install "codacy-cli.sh" => "codacy-cli"
