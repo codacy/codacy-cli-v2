1c711f2737d0697cb04c6565111a6548f1799599
heliocodacy <46491761+heliocodacy@users.noreply.github.com>
2024-12-03 17:25:02 +0000

    [skip-ci] Update codacy-cli-v2.rb

--- Formula/codacy-cli-v2.rb
+++ Formula/codacy-cli-v2.rb
@@ -1,6 +1,6 @@
 class CodacyCliV2 < Formula
-  version "0.1.0-main.105.90f7e15"
-  url "https://raw.githubusercontent.com/codacy/codacy-cli-v2/0.1.0-main.105.90f7e15/codacy-cli.sh"
+  version "0.1.0-main.107.69c755d"
+  url "https://raw.githubusercontent.com/codacy/codacy-cli-v2/0.1.0-main.107.69c755d/codacy-cli.sh"
   sha256 "fb616e2f5c639985566c81a6e6ce51db2e8de56bf217e837d13efe2f3ccc3042"
 
   def install
