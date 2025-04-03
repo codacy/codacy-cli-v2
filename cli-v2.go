359d2050722953e42c9fcec081aff103afb41ec2
andrzej.janczak <andrzej.janczak@codacy.com>
2025-04-03 10:18:53 +0200

    allow for showing help without codacy conf

--- cli-v2.go
+++ cli-v2.go
@@ -15,8 +15,22 @@ func main() {
 	// This also setup the config global !
 	configErr := config_file.ReadConfigFile(config.Config.ProjectConfigFile())
 
-	// whenever there is no configuration file, the only command allowed to run is the 'init'
-	if configErr != nil && len(os.Args) > 1 && os.Args[1] != "init" {
+	// Show help if any argument contains help
+	for _, arg := range os.Args {
+		if arg == "--help" || arg == "-h" || arg == "help" {
+			cmd.Execute()
+			return
+		}
+	}
+
+	// Check if command is init
+	if len(os.Args) > 1 && os.Args[1] == "init" {
+		cmd.Execute()
+		return
+	}
+
+	// All other commands require a configuration file
+	if configErr != nil && len(os.Args) > 1 {
 		fmt.Println("No configuration file was found, execute init command first.")
 		return
 	}
