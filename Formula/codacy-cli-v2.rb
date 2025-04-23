d665244705927e600d05d3e76ac44fcbbc337037
Joao Machado <13315199+machadoit@users.noreply.github.com>
2025-04-23 18:05:54 +0200

    Bump brew formula

--- Formula/codacy-cli-v2.rb
+++ Formula/codacy-cli-v2.rb
@@ -1,7 +1,7 @@
 class CodacyCliV2 < Formula
-  version "1.0.0-main.245.02ac1ee"
-  url "https://raw.githubusercontent.com/codacy/codacy-cli-v2/1.0.0-main.245.02ac1ee/codacy-cli.sh"
-  sha256 "25310825948b5a337a0eb13ef9cd7d52f87f0be5d99b04bacf24a25c97e391a2"
+  version "1.0.0-main.248.b0b491e"
+  url "https://raw.githubusercontent.com/codacy/codacy-cli-v2/1.0.0-main.248.b0b491e/codacy-cli.sh"
+  sha256 "6e1ec511be3c77d49b05b857e0258fc5251507fdf3499b01cd9f2c22c429b1a9"
 
   def install
     bin.install "codacy-cli.sh" => "codacy-cli"
