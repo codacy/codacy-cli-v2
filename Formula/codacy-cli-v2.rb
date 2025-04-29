2fc521f80159c302f48e416bc1ce098915773a47
franciscoazevedo <francisco.azevedo@codacy.com>
2025-04-29 12:43:15 +0100

    updated formula

--- Formula/codacy-cli-v2.rb
+++ Formula/codacy-cli-v2.rb
@@ -1,6 +1,6 @@
 class CodacyCliV2 < Formula
-  version "1.0.0-main.251.1952e73"
-  url "https://raw.githubusercontent.com/codacy/codacy-cli-v2/1.0.0-main.251.1952e73/codacy-cli.sh"
+  version "1.0.0-main.257.9759612"
+  url "https://raw.githubusercontent.com/codacy/codacy-cli-v2/1.0.0-main.257.9759612/codacy-cli.sh"
   sha256 "def852e43b05871b1a5e14dc8fb32015d89e4f4ed33fc3de8570738e6bd180f2"
 
   def install
