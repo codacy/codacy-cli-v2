5cc2b0a9d272dcee65b464883362ad52ae8504aa
zhamborova <47396369+zhamborova@users.noreply.github.com>
2025-05-08 15:45:45 +0200

    Update formula (#108)

--- Formula/codacy-cli-v2.rb
+++ Formula/codacy-cli-v2.rb
@@ -1,6 +1,6 @@
 class CodacyCliV2 < Formula
-  version "1.0.0-main.271.4770bfe"
-  url "https://raw.githubusercontent.com/codacy/codacy-cli-v2/1.0.0-main.271.4770bfe/codacy-cli.sh"
+  version "1.0.0-main.275.bc32470"
+  url "https://raw.githubusercontent.com/codacy/codacy-cli-v2/1.0.0-main.275.bc32470/codacy-cli.sh"
   sha256 "def852e43b05871b1a5e14dc8fb32015d89e4f4ed33fc3de8570738e6bd180f2"
 
   def install
