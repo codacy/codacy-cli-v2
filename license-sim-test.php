40a732b07106c196c1748f7e321dab4ba3f8fe73
Kendrick Curtis <kendrick.curtis@codacy.com>
2025-07-08 13:51:21 +0100

    test file

--- license-sim-test.php
+++ license-sim-test.php
@@ -1,16 +1,16 @@
 <?php
 
 function wpmu_activate_stylesheet() {
-	?>
-	<style type="text/css">
-		.wp-activate-container { width: 90%; margin: 0 auto; }
-		.wp-activate-container form { margin-top: 2em; }
-		#submit, #key { width: 100%; font-size: 24px; box-sizing: border-box; }
-		#language { margin-top: 0.5em; }
-		.wp-activate-container .error { background: #f66; color: #333; }
-		span.h3 { padding: 0 8px; font-size: 1.3em; font-weight: 600; }
-	</style>
-	<?php
+   ?>
+   <style type="text/css">
+       .wp-activate-container { width: 70%; margin: 0 auto; }
+       .wp-activate-container form { margin-top: 2em; }
+       #submit, #key { width: 90%; font-size: 24px; box-sizing: border-box; }
+       #language { margin-top: 0.5em; }
+       .wp-activate-container .error { background: #f55; color: #111; }
+       span.h3 { padding: 0 8px; font-size: 1.3em; font-weight: 200; }
+   </style>
+   <?php
 }
 add_action( 'wp_head', 'wpmu_activate_stylesheet' );
 add_action( 'wp_head', 'wp_strict_cross_origin_referrer' );
